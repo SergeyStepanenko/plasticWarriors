ab676984c183430f6eaa15838679a801fdaaf84e
Sergey <groverfmx@gmail.com>
2017-12-10 17:49:30 +0300

    Added pin svg

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
 import * as firebase from 'firebase';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
@@ -6,7 +7,6 @@ import * as B from 'react-bootstrap';
 import { CirclePicker } from 'react-color';
 
 import mapImg from './maps/map_1.jpg';
-// import mapKML from './maps/map.kml';
 
 const config = {
 	apiKey: 'AIzaSyCB1TfuGQegOrHOPcFJFqpxDmMTSElXQVg',
@@ -40,7 +40,6 @@ export default class App extends PureComponent {
 		},
 		positionedWarriors: [],
 		isTrackingDataLoading: true,
-		isFirebaseDataLoading: true,
 	}
 
 	componentDidMount() {
@@ -49,11 +48,11 @@ export default class App extends PureComponent {
 				firebaseData: snap.val()
 			}, () => {
 				this.setState({ isFirebaseDataLoading: false });
-				this.requestAndPaintData();
+				this.requestAndHandleResponse();
 
 				// this.interval = setInterval(() => {
 				// 	console.log('request');
-				// 	this.requestAndPaintData();
+				// 	this.requestAndHandleResponse();
 				// }, 30000);
 			});
 		});
@@ -196,7 +195,7 @@ export default class App extends PureComponent {
 		});
 	}
 
-	requestAndPaintData = () => {
+	requestAndHandleResponse = () => {
 		this.setState({
 			imgParams: this.$image.getBoundingClientRect(),
 			isTrackingDataLoading: true,
@@ -214,10 +213,10 @@ export default class App extends PureComponent {
 			form,
 			positionedWarriors,
 			isTrackingDataLoading,
-			isFirebaseDataLoading,
+			// isFirebaseDataLoading,
 		} = this.state;
 
-		const isDisabled = ~Object.values(form).indexOf('');
+		const isFormCompleted = !!~Object.values(form).indexOf('');
 
 		return (
 			<div>
@@ -230,12 +229,14 @@ export default class App extends PureComponent {
 					</div>
 					{
 						positionedWarriors.map((warrior) => {
+							const iconWidth = 15;
+							const iconHeight = 15;
+							const shiftOnMap = `translate(${warrior.lngInPx - iconWidth / 2}px, ${warrior.ltdInPx - iconHeight / 2}px)`;
 							const style = {
 								position: 'relative',
-								backgroundColor: warrior.color,
-								width: '10px',
-								height: '10px',
-								transform: `translate(${warrior.lngInPx}px, ${warrior.ltdInPx}px)`,
+								width: `${iconWidth}px`,
+								height: `${iconHeight}px`,
+								transform: shiftOnMap,
 							};
 
 							const tooltip = (
@@ -250,7 +251,9 @@ export default class App extends PureComponent {
 							return (
 								<div key={`${warrior.name} ${warrior.key}`}>
 									<B.OverlayTrigger id={warrior.key} overlay={tooltip}>
-										<div style={style}/>
+										<div style={style}>
+											<PinSVG color={warrior.color} />
+										</div>
 									</B.OverlayTrigger>
 								</div>
 							);
@@ -299,9 +302,9 @@ export default class App extends PureComponent {
 									<B.Button
 										type="submit"
 										onClick={this.handleSubmit}
-										disabled={isDisabled}
+										disabled={isFormCompleted}
 									>
-										{isDisabled ? 'Добавить' : 'В процессе'}
+										Добавить
 									</B.Button>
 								</B.Col>
 							</B.FormGroup>
@@ -314,7 +317,13 @@ export default class App extends PureComponent {
 									return(
 										<B.Row key={key}>
 											<B.Col sm={3}>{firebaseData[key].name}</B.Col>
-											<B.Col sm={2}>{firebaseData[key].color || 'color'}</B.Col>
+											<B.Col sm={2}>
+												<PinSVG
+													color={firebaseData[key].color}
+													width='15px'
+													height='15px'
+												/>
+											</B.Col>
 											<B.Col sm={2}>{firebaseData[key].status || 'isOnline'}</B.Col>
 										</B.Row>
 									);
@@ -337,7 +346,7 @@ export default class App extends PureComponent {
 						</B.FormGroup>
 					</B.Panel>
 					<B.Button
-						onClick={this.requestAndPaintData}
+						onClick={this.requestAndHandleResponse}
 						disabled={isTrackingDataLoading}
 					>
 						{isTrackingDataLoading ? 'Обновляется' : 'Обновить карту'}
@@ -347,3 +356,20 @@ export default class App extends PureComponent {
 		);
 	}
 }
+
+export function PinSVG({ width = '100%', height = '100%', color }) {
+	return (
+		<svg xmlns="http://www.w3.org/2000/svg" version="1.1" id="Capa_1" x="0px" y="0px" viewBox="0 0 426.667 426.667" width={width} height={height}>
+			<g>
+				<path d="M213.333,106.667c-58.88,0-106.667,47.787-106.667,106.667S154.453,320,213.333,320S320,272.213,320,213.333     S272.213,106.667,213.333,106.667z" fill={color}/>
+				<path d="M213.333,0C95.467,0,0,95.467,0,213.333s95.467,213.333,213.333,213.333S426.667,331.2,426.667,213.333     S331.2,0,213.333,0z M213.333,384c-94.293,0-170.667-76.373-170.667-170.667S119.04,42.667,213.333,42.667     S384,119.04,384,213.333S307.627,384,213.333,384z" fill={color}/>
+			</g>
+		</svg>
+	);
+}
+
+PinSVG.propTypes = {
+	width: PropTypes.string,
+	height: PropTypes.string,
+	color: PropTypes.string,
+};

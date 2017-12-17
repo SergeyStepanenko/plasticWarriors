bcd6b24ae2271b88641b01bb37a563fc1af284c9
Sergey <groverfmx@gmail.com>
2017-12-17 19:35:50 +0300

    build upd

--- src/_blocks/panel/index.js
+++ src/_blocks/panel/index.js
@@ -12,65 +12,84 @@ export default class Panel extends PureComponent {
 		admin: PropTypes.bool,
 	}
 
+	getMinutesAgo = (milliseconds) => {
+		if (!milliseconds) {
+			return false;
+		}
+
+		if (milliseconds > Date.now()) {
+			console.error('Время сервера опережает браузерное', milliseconds - Date.now());
+		}
+
+		const minutes = (Date.now() - milliseconds) / 1000 / 60;
+		const seconds = minutes < 1 ? Math.round(minutes * 60) : false;
+
+
+		return minutes < 1 ? seconds : Math.round(minutes);
+	}
+
 	render() {
 		const { units, positionedWarriors, admin } = this.props;
+		const LOADING = 'loading';
+		let fbArray = Object.keys(units).map(value => ({ ...units[value] }));
+
+		if (positionedWarriors.length) {
+			fbArray = positionedWarriors;
+		}
 
 		return (
 			<div>
-				<B.Panel header={'Пластиковые воины:'} bsStyle="primary">
+				<B.Panel header={`Статистика: Всего - ${fbArray.length}`} bsStyle="primary">
 					<B.FormGroup className='app__panel'>
-						<div>
-							{
-								Object.keys(units).map((key) => {
-									return(
-										<div key={key} className='app__block'>
-											<div className='app__block-name'>{units[key].name}</div>
-											<B.Col sm={1}>
-												<PinSVG
-													color={units[key].color}
-													width='15px'
-													height='15px'
-												/>
-											</B.Col>
-											{
-												admin &&
-													<B.Col sm={3}>
-														<B.Button onClick={() => this.props.editWarrior({ units, key })}>
-															Изменить
-														</B.Button>
-													</B.Col>
-											}
-											{
-												admin &&
-													<B.Col sm={3}>
-														<B.Button onClick={() => this.props.deleteWarrior(key)}>
-															Удалить
-														</B.Button>
-													</B.Col>
-											}
-										</div>
-									);
-								})
-							}
-						</div>
-					</B.FormGroup>
-				</B.Panel>
-				<B.Panel header={'Геоданные:'} bsStyle="primary">
-					<B.FormGroup className='app__panel'>
-						{
-							positionedWarriors && positionedWarriors.map(warrior => (
-								!warrior.error &&
-									<div key={warrior.key || Math.random()} /* className='app__block' */>
-										<B.Col sm={3}>
-											<div>{warrior.name}</div>
-											<div>{`lat:${warrior.lat}`}</div>
-											<div>{`lng:${warrior.lng}`}</div>
-											<div>{warrior.isInRange ? 'На карте' : 'Вне карты'}</div>
-											<div>{`${new Date(warrior.time).getHours()}:${new Date(warrior.time).getMinutes()}`}</div>
-										</B.Col>
-									</div>
-							))
-						}
+						<table>
+							<tbody>
+								{
+									fbArray.map((warrior) => {
+										const timeAgo = this.getMinutesAgo(warrior.time);
+										const status = timeAgo > 5 && 'Оффлайн';
+										const accuracy = warrior.acc && `${warrior.acc}m` || LOADING;
+										const batteryLevel = warrior.batteryLvl && `${warrior.batteryLvl}%` || LOADING;
+										const timeAgoString = timeAgo > 1 ? `${timeAgo}мин` : '<1мин';
+
+										return (
+											<tr key={warrior.url}>
+												<td>
+													<PinSVG
+														color={warrior.color}
+														width='15px'
+														height='15px'
+													/>
+												</td>
+												<td>{warrior.name || LOADING}</td>
+												<td>{timeAgoString}</td>
+												<td>{batteryLevel}</td>
+												<td>{accuracy}</td>
+												<td className='app__panel-status'>{status}</td>
+												<td>
+													<a href={warrior.url} target='_blank'>Ссылка</a>
+												</td>
+												{
+													admin &&
+														<td>
+															<B.Button onClick={() => this.props.editWarrior({ units, key: warrior.key })}>
+																Изменить
+															</B.Button>
+														</td>
+												}
+												{
+													admin &&
+														<td>
+															<B.Button onClick={() => this.props.deleteWarrior(warrior.key)}>
+																Удалить
+															</B.Button>
+														</td>
+												}
+											</tr>
+										);
+									})
+								}
+							</tbody>
+						</table>
 					</B.FormGroup>
 				</B.Panel>
 			</div>

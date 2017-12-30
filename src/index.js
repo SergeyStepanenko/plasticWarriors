4dcbd812bcdd71f17f2f15a79b53974717fe1437
Sergey <groverfmx@gmail.com>
2017-12-30 19:18:21 +0300

    add

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
 
 import App from './app';
@@ -9,10 +9,10 @@ import './styles.css';
 
 ReactDOM.render(
 	<BrowserRouter>
-		<div>
-			<Route exact path="/" component={App}/>
-			<Route path="/addUnit" component={WarriorAddPage}/>
-		</div>
+		<Switch>
+			<Route exact path='/' component={App}/>
+			<Route path='/add' component={WarriorAddPage}/>
+		</Switch>
 	</BrowserRouter>,
 	document.getElementById('root'));
 registerServiceWorker();
